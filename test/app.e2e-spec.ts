71d5c153ee3be819ab6fc6cd8b8d6496a8a9b051
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-11 19:00:14 +0700

    yarn test:e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Person } from './../src/model/person';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,9 +17,12 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
+    const arif = new Person()
+    arif.name = 'Arif Ikhsanudin'
+
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(JSON.stringify(arif));
   });
 });
